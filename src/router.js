196e5b75876e663d311cf8c43a56524d8ae3f6fc
czq <chenzhengqiang@youzan.com>
2019-11-27 18:07:14 +0800

    fix: 保留注释

--- src/router.js
+++ src/router.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Route, Switch, HashRouter } from 'react-router-dom';
 import Home from './pages/home';
 
+// const Home = suspenseContainer(lazy(() => import('./pages/home')));
+
+// function suspenseContainer(Component) {
+//   return (props) => (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Component {...props} />
+//     </Suspense>
+//   );
+// }
+
 const routes = () => (
   <HashRouter>
     <Switch>
