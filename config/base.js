76d80c0865058b5a4d9c44ac6b4b7c4494d44c27
lear <1325925707@qq.com>
2019-12-15 14:55:36 +0800

    fix: 优化打包

--- config/base.js
+++ config/base.js
@@ -1,6 +1,14 @@
-
 exports.STATIC_PATH = '../public';
 
 exports.DIST_PATH = '../dist';
 
 exports.MAIN_JS = '../src/main.tsx';
+
+exports.DLL_LIST = [
+  'react',
+  'react-dom',
+  'react-router-dom',
+  'axios',
+  'classnames',
+  'safe-area-insets',
+];
--- config/webpack.dll.js
+++ config/webpack.dll.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const { DIST_PATH } = require('./base');
+const { DIST_PATH, DLL_LIST } = require('./base');
 
 module.exports = {
   // 要打包的模块的数组
   context: __dirname,
   entry: {
-    vendor: ['react', 'react-dom', 'react-router-dom']
+    vendor: DLL_LIST,
   },
   output: {
     path: path.join(__dirname, DIST_PATH), // 打包后文件输出的位置
     filename: 'dll/[name].dll.js', // vendor.dll.js中暴露出的全局变量名。
-    library: '[name]_library' // 与webpack.DllPlugin中的`name: '[name]_library',`保持一致。
+    library: '[name]_library', // 与webpack.DllPlugin中的`name: '[name]_library',`保持一致。
   },
   plugins: [
     new webpack.DllPlugin({
       path: path.join(__dirname, DIST_PATH, '[name]-manifest.json'),
       name: '[name]_library',
-      context: __dirname
+      context: __dirname,
     }),
-    new CompressionWebpackPlugin()
-  ]
+    new CompressionWebpackPlugin(),
+  ],
 };
