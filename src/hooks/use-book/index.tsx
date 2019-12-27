da1ea93f0675aaed3334d9300ae3f19d6cb31e4f
lear <1325925707@qq.com>
2019-12-27 21:42:12 +0800

    fix: 更新参数

--- src/hooks/use-book/index.tsx
+++ src/hooks/use-book/index.tsx
@@ -20,7 +20,7 @@ const useBookAndFlatten = () => {
     {
       bookName: '天醒之路',
       author: '蝴蝶蓝',
-      img: 'http://www.xs.la/BookFiles/BookImages/64.jpg',
+      img: 'https://www.xinxs.la/BookFiles/BookImages/64.jpg',
       desc:
         '“路平，起床上课。”\n“再睡五分钟。”\n“给我起来！”\n哗！阳光洒下，照遍路平全身。\n“啊！！！”惊叫声顿时响彻云霄，将路平的睡意彻底击碎，之后已是苏唐摔门而出的怒吼：“什么条件啊你玩裸睡？！”\n......',
       latestChapter: '上架感言!',
@@ -29,7 +29,7 @@ const useBookAndFlatten = () => {
       isUpdate: false,
       updateNum: 0,
       source: {
-        '1': 'http://www.xs.la/0_64/',
+        '1': 'https://www.xinxs.la/0_64/',
         '2': 'http://www.kanshuzhong.com/book/36456/',
       },
     },
