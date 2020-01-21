ef315baa31befa9e794f9bc38f6059ec8905aca2
czq <chenzhengqiang@youzan.com>
2020-01-21 15:45:01 +0800

    fix: 聚合代码, 避免琐碎的内容产生

--- src/pages/read/components/new-reader/index.tsx
+++ src/pages/read/components/new-reader/index.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import Content from './content';
-import Title from './title';
+import { useReaderContext } from '../../context';
+import useDrag from '../../hook/use-drag';
 
 import styles from './index.m.scss';
-import { useReaderContext } from '../../context';
 
 const NewReader: React.FC = () => {
-  const { title } = useReaderContext();
+  const { title, pages, watched, nextChapter, prevChapter } = useReaderContext();
+  const { ref, page, touchEvent, total } = useDrag(pages, {
+    initPage: watched,
+    nextChapter,
+    prevChapter,
+  });
+
+  const footer = useMemo(() => {
+    if (total < 1) return null;
+    return (
+      <div className={styles.footer}>
+        {page + 1}/{total}
+      </div>
+    );
+  }, [total, page]);
+
   return (
     <div className={styles.container}>
-      <Title name={title} />
-      <Content />
+      <div className={styles.title}>{title}</div>
+      <div className={styles.wrapper}>
+        <div className={styles.inner} {...touchEvent}>
+          <div ref={ref} className={styles.main}>
+            {pages.map((i: string, ind: number) => (
+              <p key={'asd_x' + ind}>{i}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      {footer}
     </div>
   );
 };
