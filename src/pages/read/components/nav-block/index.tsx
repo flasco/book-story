34cc31df48fdac71f54ff27dab55890e1fab3a50
czq <chenzhengqiang@youzan.com>
2020-09-25 13:19:17 +0800

    fix: 修复触控不到的问题

--- src/pages/read/components/nav-block/index.tsx
+++ src/pages/read/components/nav-block/index.tsx
@@ -61,7 +61,9 @@ const NavBlock = () => {
       <CatalogDrawer open={catalog} changeOpen={changeCatalog} changeMenu={changeMenu}>
         <div className={styles.container}>
           <div className={styles.header}>
-            <Icon type="left" className={styles.back} onClick={goBack} />
+            <div className={styles.back} onClick={() => goBack()}>
+              <Icon type="left" />
+            </div>
           </div>
           <Touchable needStop className={styles.content} onClick={() => changeMenu()} />
           {/**TODO: 状态机，同一时间内只有一个面板展示 */}
