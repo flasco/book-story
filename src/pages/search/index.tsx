4cd8bedd8391e492f0e33623af773f1cc7607dec
czq <chenzhengqiang@youzan.com>
2020-11-20 16:22:48 +0800

    fix: update

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { SearchBar } from 'antd-mobile';
 
 import Container from '@/layout/container';
