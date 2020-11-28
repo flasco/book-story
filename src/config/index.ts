a0860264c41725f3aeed0e2bf24a64215b98420f
flasco <flasco@163.com>
2020-11-28 21:17:54 +0800

    fix: use international

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,10 @@
 const isDev = process.env.PROJECT_ENV === 'development';
 
 const serverIps = [
-  'https://cmdsh3b7eebg.cn-e1.leanapp.cn',
-  'https://mhbxymrllauo.cn-e1.leanapp.cn',
+  'https://tassel-1.avosapps.us',
+  'https://tassel-2.avosapps.us',
+  // 'https://cmdsh3b7eebg.cn-e1.leanapp.cn',
+  // 'https://mhbxymrllauo.cn-e1.leanapp.cn',
 ];
 
 export const getIp = (() => {
@@ -13,7 +15,7 @@ export const getIp = (() => {
     if (isDev) return 'http://localhost:3001';
     const current = Date.now();
     if (prevIp == null || current - lockTime > 3600 * 1000) {
-      const currenthours = new Date().getHours();
+      const currenthours = new Date().getUTCHours() + 8;
       const curPoi = currenthours >= 9 && currenthours < 22 ? 0 : 1;
       prevIp = serverIps[curPoi];
       lockTime = current;
