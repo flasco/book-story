372bc8c40e3a4f66971fc898cb4465fea5d260c9
czq <chenzhengqiang@youzan.com>
2021-02-05 16:11:19 +0800

    fix: 优化,修复报错

--- src/components/image-show/index.tsx
+++ src/components/image-show/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 
 import defaultImage from '@/assets/noImg.jpg';
 
@@ -7,15 +7,21 @@ import { getImage } from './api';
 // 考虑做个图片本地缓存
 const ImageShow: React.FC<any> = ({ src, ...otherProps }) => {
   const [origin, setSrc] = useState(null);
+  const isActive = useRef(true);
 
   useLayoutEffect(() => {
     getImage(src)
       .then(image => {
-        setSrc(image);
+        if (isActive.current) {
+          setSrc(image);
+        }
       })
       .catch(() => {
         setSrc(defaultImage);
       });
+    return () => {
+      isActive.current = false;
+    };
   }, [src]);
 
   return React.createElement('img', { ...otherProps, src: origin });
