d455d1b39585c747c78dc3c8af6d287bd8413528
flasco <flasco@163.com>
2021-10-03 00:01:30 +0800

    fix: 修订错误

--- src/pages/detail/api/index.ts
+++ src/pages/detail/api/index.ts
@@ -2,5 +2,5 @@ import { post } from '@/utils/request';
 
 /** 书源竞争，判断使用的书源优先级 */
 export async function getDetail(sources: string[]) {
-  return post<any[]>('/v3/analyze/api/infos', sources);
+  return post<any[]>('/v3/books/api/infos', sources);
 }
--- src/pages/search/api/index.ts
+++ src/pages/search/api/index.ts
@@ -1,9 +1,15 @@
 import { get } from '@/utils/request';
 
 export function newSearch(keyword: string) {
-  return get('/v3/books/api/search', {
-    keyword,
-  });
+  return get(
+    '/v3/books/api/search',
+    {
+      keyword,
+    },
+    {
+      cache: false,
+    }
+  );
 }
 
 export function getSites() {
