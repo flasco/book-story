06b9d1c4cda456c72923fb60b025bbe022239c6c
flasco <flasco@163.com>
2022-07-29 18:29:38 +0800

    fix: update to vite@3

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
 // import legacy from '@vitejs/plugin-legacy';
 import macrosPlugin from 'vite-plugin-babel-macros';
-import OptimizationPersist from 'vite-plugin-optimize-persist';
-import PkgConfig from 'vite-plugin-package-config';
 // import { visualizer as analyze } from 'rollup-plugin-visualizer';
 
 import { version } from './package.json';
@@ -18,8 +16,6 @@ export default defineConfig({
       include: '**/*.tsx',
     }),
     macrosPlugin(),
-    PkgConfig(),
-    OptimizationPersist(),
     viteCompression({
       filter: /\.(js|css)$/i,
     }),
@@ -37,6 +33,10 @@ export default defineConfig({
       // plugins: [analyze()],
     },
   },
+  // optimizeDeps: {
+  //   disabled: false,
+  //   devScan: true
+  // },
   define: {
     ENV: JSON.stringify({
       PROJECT_VERSION_TAG: `${version}（${new Date().toLocaleDateString('zh')}）`,
@@ -44,6 +44,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, 'src'),
       lodash: 'lodash-es',
     },
